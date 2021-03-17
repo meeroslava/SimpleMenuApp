0f9c8c8c25188bca22e5b60c457feef5cbcaf25e
meeroslava <sapphirenymph@gmail.com>
2021-03-17 22:36:59 +0200

    main page, details page, and initial edit page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
+import { EditPageComponent } from './components/edit-page/edit-page.component';
+import { RestaurantService } from './services/restaurant.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'rxjs';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent },
+  {
+    path: ':id',
+    component: DetailsPageComponent,
+  },
+  {
+    path: ':id/edit',
+    component: EditPageComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    DetailsPageComponent,
+    EditPageComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTableModule,
+    MatListModule,
+    MatIconModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [RestaurantService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
